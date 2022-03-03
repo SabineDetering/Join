53da48877df517b8c5a8c2159d4d0b82e2b09771
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-03 10:36:08 +0100

    all menues added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function login(){
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('logout').style.display = 'flex';
+
+}
+
+function logout(){
+    document.getElementById('login').style.display = 'flex';
+    document.getElementById('logout').style.display = 'none';
+
+}
