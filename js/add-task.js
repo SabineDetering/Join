36569aa658ea98acc8d5061af26b5fd760e3c8ac
Sabine Detering <sabine.detering@web.de>
2022-03-03 19:00:27 +0100

    radio button in add-task.html

--- js/add-task.js
+++ js/add-task.js
@@ -1,7 +1,6 @@
 function addTask(status) {
     let currentTask = getTaskData();
     currentTask.id = allTasks.length + 1;
-    currentTask.status = status;
     allTasks.push(currentTask);
     save(allTasks);
 }
@@ -10,11 +9,16 @@ function getTaskData() {
     let description = getId('description').value;
     let catSelector = getId('category');
     let category = catSelector[catSelector.selectedIndex].value;
-    let dueDate = getId('due-date').value;
+    let dueDate = getId('due-date').value;//format "yyyy-mm-dd"
     let impSelector = getId('importance');
     let importance = impSelector[impSelector.selectedIndex].value;
     let assignSelector = getId('assigned-to');
     let assignedTo = [];
     assignedTo.push(assignSelector[assignSelector.selectedIndex].value);
-    return { title, description, category, dueDate, importance, assignedTo}
+    let status = 'backlog';
+    let statusToDo = getId('statusToDo');
+    if (statusToDo.checked) {
+        status = 'todo';
+    } 
+    return { title, description, category, dueDate, importance, assignedTo,status}
 }
