92781bd8c8eb5941574fec97bab8e86348207fc9
Sabine Detering <sabine.detering@web.de>
2022-03-03 14:51:38 +0100

    functions to retrieve data from add-task.html

--- js/add-task.js
+++ js/add-task.js
@@ -1,9 +1,20 @@
 function addTask(status) {
     let currentTask = getTaskData();
+    currentTask.id = allTasks.length + 1;
     currentTask.status = status;
     allTasks.push(currentTask);
     save(allTasks);
 }
 function getTaskData() {
-    
+    let title = getId('title').value;
+    let description = getId('description').value;
+    let catSelector = getId('category');
+    let category = catSelector[catSelector.selectedIndex].value;
+    let dueDate = getId('due-date').value;
+    let impSelector = getId('importance');
+    let importance = impSelector[impSelector.selectedIndex].value;
+    let assignSelector = getId('assigned-to');
+    let assignedTo = [];
+    assignedTo.push(assignSelector[assignSelector.selectedIndex].value);
+    return { title, description, category, dueDate, importance, assignedTo}
 }
