60922c9ce0ffb56cff0e34109a704bbb555860b1
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-04 19:48:30 +0100

    push

--- js/board.js
+++ js/board.js
@@ -1,15 +1,18 @@
 
 async function getBoardTasks(){
-    
+    let response = await fetch(allTasks)
+    // let jsonTask = await response.json();
+    renderCompleteTask(response)
+
 
 }
 
-function renderCompleteTask() {
+function renderCompleteTask(response) {
 
-    for (let i = 0; i < allTasks.length; i++) {
-        const task = allTasks[i];
+    for (let i = 0; i < response.length; i++) {
+        const task = response[i];
 
-        getId('task').innerHTML =`
+        getId('task').innerHTML +=`
         <span id="title${i}">${task['title']}</span>
         <span id="description${i}">${task['description']}</span>
         <span id="category${i}">${task['category']}</span>
@@ -17,7 +20,6 @@ function renderCompleteTask() {
         <span id="importance${i}">${task['importance']}</span>
         <span id="assigned${i}">${task['assignedTo']}</span>
         <span id="status${i}">${task['status']}</span>
-
         `;
     }
 }
