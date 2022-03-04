2bfc09f6e74bb19118b5b24d0ae32ba0f7c68750
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-03-04 15:05:31 +0100

    changed name of backend

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,4 @@
-setURL('http://developerakademie.com/smallest_backend_ever-master')
+setURL('http://developerakademie.com/smallest_backend_ever')
 
 async function init() {
     await downloadFromServer();
