91fa618e60ffdf12856f7bc94a5de0c6034706ab
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-04 19:18:41 +0100

    script

--- js/board.js
+++ js/board.js
@@ -1,9 +1,17 @@
-function renderCompleteTask(){
-    // getId('title').innerHTML = allTasks[0]['title'];
-    // getId('description').innerHTML = allTasks[0]['description'];
-    // getId('category').innerHTML = allTasks[0]['category'];
-    // getId('date').innerHTML = allTasks[0]['dueDate'];
-    // getId('importance').innerHTML = allTasks[0]['importance'];
-    // getId('assigned').innerHTML = allTasks[0]['assignedTo'];
-    // getId('status').innerHTML = allTasks[0]['status'];
+async function renderCompleteTask() {
+    
+    for (let i = 0; i < allTasks.length; i++) {
+        const task = allTasks[i];
+
+        getId('task').innerHTML =`
+        <span id="title${i}">${task['title']}</span>
+        <span id="description${i}">${task['description']}</span>
+        <span id="category${i}">${task['category']}</span>
+        <span id="date${i}">${task['dueDate']}</span>
+        <span id="importance${i}">${task['importance']}</span>
+        <span id="assigned${i}">${task['assignedTo']}</span>
+        <span id="status${i}">${task['status']}</span>
+
+        `;
+    }
 }
