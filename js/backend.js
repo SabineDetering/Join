95dfbb5e2b7567ba7cd8a6060f688cd8893960c6
Sabine Detering <sabine.detering@web.de>
2022-03-04 17:34:58 +0100

    backend initialisation moved back to backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,1 +1,6 @@
+setURL('http://gruppe-194.developerakademie.net/smallest_backend_ever');
 
+async function init() {
+    await downloadFromServer();
+    allTasks = JSON.parse(backend.getItem('tasks')) || [];
+}
