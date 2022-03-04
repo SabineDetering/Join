37d54cebc603746b33be65f341e66f1ee96b4341
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-04 12:45:39 +0100

    board.js added

---
+++ js/board.js
@@ -0,0 +1,11 @@
+function renderCompleteTask(){
+    getId('title').innerHTML = allTasks[0]['title'];
+    getId('description').innerHTML = allTasks[0]['title'];
+    getId('category').innerHTML = allTasks[0]['title'];
+    getId('date').innerHTML = allTasks[0]['title'];
+    getId('importance').innerHTML = allTasks[0]['title'];
+    getId('assigned').innerHTML = allTasks[0]['title'];
+    getId('status').innerHTML = allTasks[0]['title'];
+
+
+}
