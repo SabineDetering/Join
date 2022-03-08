5177e3105ce83f6ea87b8c84f858bbf6e548d933
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-08 20:00:16 +0100

    sign-in only with guest checked

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-function signIn(){
-    if (getId('guest').value == 'Sign-in as Guest') {
+function signIn() {
+    if (getId('guest').checked) {
         location.href = 'http://127.0.0.1:5500/board.html?'
+    } if (!getId('guest').checked) {
+        alert('Please Sign-In as Guest')
     }
-    
 }
