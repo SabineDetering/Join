e3455c68b67527ece4c32c344415396e83174b63
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-08 19:41:53 +0100

    sign in clickable

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-// function signIn(){
-//     if (users) {
-        
-//     }
-// }
+function signIn(){
+    if (getId('guest').value == 'Sign-in as Guest') {
+        location.href = 'http://127.0.0.1:5500/board.html?'
+    }
+    
+}
