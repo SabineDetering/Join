4c2366cb304e9f0c024c143b0209947b00a01713
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-09 13:30:42 +0100

    Users Sabine Christian Tuncay added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 function signIn() {
 
-    if (getId('guest').checked || getId('login-email').value == users['Tuncay Dağdelen']['email'] && getId('login-passwort').value == users['Tuncay Dağdelen']['password']) {
+    if (getId('guest').checked || (getId('login-email').value == users['Sabine Detering']['email'] && getId('login-passwort').value == users['Sabine Detering']['password']) || (getId('login-email').value == users['Christian Aidelsburger']['email'] && getId('login-passwort').value == users['Christian Aidelsburger']['password']) || (getId('login-email').value == users['Tuncay Dağdelen']['email'] && getId('login-passwort').value == users['Tuncay Dağdelen']['password'])) {
         location.href = 'http://127.0.0.1:5500/board.html?'
+
+
     } else {
         alert('Please Sign-In as Guest or Log-In')
     }
+
 }
+
+
+// for (let i = 0; i < users.length; i++) {
+//     let element = users[i];
+//     let email = element['name']
+//     let password = element['password']
+
+    
+// }
