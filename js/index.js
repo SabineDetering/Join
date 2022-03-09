eb11337c9e1ffb50f9047833ca555f4293a15e8f
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-09 11:34:08 +0100

    User Tuncay Added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 function signIn() {
-    if (getId('guest').checked) {
+
+    if (getId('guest').checked || getId('login-email').value == users['Tuncay Dağdelen']['email'] && getId('login-passwort').value == users['Tuncay Dağdelen']['password']) {
         location.href = 'http://127.0.0.1:5500/board.html?'
-    } if (!getId('guest').checked) {
-        alert('Please Sign-In as Guest')
+    } else {
+        alert('Please Sign-In as Guest or Log-In')
     }
 }
