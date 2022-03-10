d040f1a7062b469860c02d29ae4c13a7dc79c25a
Sabine Detering <sabine.detering@web.de>
2022-03-10 16:38:26 +0100

    responsive menu

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
+
 function signIn() {
+    let email = getId('login-email').value;
+    let password = getId('login-passwort').value;
+    let userFound = false;
+    let checkBox = getId('guest');
 
-    if (getId('guest').checked || (getId('login-email').value == users['Sabine Detering']['email'] && getId('login-passwort').value == users['Sabine Detering']['password']) || (getId('login-email').value == users['Christian Aidelsburger']['email'] && getId('login-passwort').value == users['Christian Aidelsburger']['password']) || (getId('login-email').value == users['Tuncay Dağdelen']['email'] && getId('login-passwort').value == users['Tuncay Dağdelen']['password'])) {
-        location.href = 'http://127.0.0.1:5500/board.html?'
-
+    if (checkBox.checked) {
+        location.href = 'board.html';
 
     } else {
-        alert('Please Sign-In as Guest or Log-In')
+        for (const name in users) {
+
+            if (email == users[name].email) {
+
+                if (password == users[name].password) {
+                    location.href = 'board.html'; userFound = true;
+
+                }
+            }
+        }
+        if (!userFound) {
+            alert('Please Sign-In as Guest or Log-In');
+        }
     }
-
 }
-
-
-// for (let i = 0; i < users.length; i++) {
-//     let element = users[i];
-//     let email = element['name']
-//     let password = element['password']
-
-    
-// }
---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    let menubar = getId('menubar');
+    menubar.classList.remove('d-none');
+}
+function hideMenu() {
+    let menubar = getId('menubar');
+    menubar.classList.add('d-none');
+}
