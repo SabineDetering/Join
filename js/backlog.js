38d2dc90301b54cb28c1035d449282f2f0cf4619
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-03-10 12:17:10 +0100

    created dynamic content in backlog card + save button

--- js/backlog.js
+++ js/backlog.js
@@ -62,86 +62,116 @@ function showCard(i) {
     contentOfCard.innerHTML = `
     <div class="modal-header">
     <h5 class="modal-title" id="title">
-    <input type="text" class="form-control" value="${task.title}">
+    <textarea id="backlog-title" class="textarea-backlog form-control">${task.title}</textarea>
     </h5>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div id="description" class="modal-body">
-    <h6><b>Description:</b></h6><textarea class="form-control">${task.description}</textarea>
+    <h6><b>Description:</b></h6><textarea id="backlog-description" class="textarea-backlog-description form-control">${task.description}</textarea>
     <div class="infosOfCard">
-    <div><h6><b>due Date:</b></h6><input value="${task.dueDate}" type="date" class="form-control mb-3" required aria-describedby="due-date-button"
-    id="due-date"></div>
+    <div class="dateAndCategory">
+    <div><h6><b style="padding-left: 12px">due Date:</b></h6><input id="backlog-date" value="${task.dueDate}" type="date" class="backlog-date form-control mb-3" required aria-describedby="due-date-button" id="due-date"></div>
+    <div><h6><b>category:</b></h6>${task.category}</div>
+    </div>
     <h6><b>assignet to:  
     
-    <div id="assigned-to-backlog">
-    <div class="btn-group dropend">
-        <img onclick="emptyForm()" id="plus-icon-backlog" type="button" class="dropdown-toggle" data-bs-toggle="dropdown"
-            aria-expanded="false" src="img/icon plus.png" alt="add team member"
-            title="add team member">
-
-        <ul id="assigned-to-list-backlog" class="dropdown-menu p-3">
-        </ul>
-    </div>
-    </div>
-
-</b></h6><div style="display: flex">${getStaff(i)}</div>
+    </b></h6><div style="display: flex">${getStaff(i)}</div>
     </div>
     </div>
     <div id="button" class="modal-footer">
-    <button onclick="nextCard(${i})" class="btn btn-primary">next Task</button>
+    <button onclick="previousCard(${i})" class="btn btn-primary">previous task</button>
+    <button onclick="saveChanges(${i})" type="button" class="btn btn-outline-success">save changes</button>
+    <button id="nextTask" onclick="nextCard(${i})" class="btn btn-primary">next task</button>
     </div>`;
 
 }
 
+function previousCard(i) {
+    if (i == 0) {
+        document.getElementById('nextTask').classList.add('background-color-grey');
+        alert('this is the first task');
+    } else {
+        i--;
+    }
+    showCard(i);
+}
+
 function nextCard(i) {
     if (i == backlogTasks.length -1) {
-        i = 0
+        alert('no more tasks available')
+        document.getElementById('nextTask').classList.add('background-color-grey');
     } else {
         i++;
     }
     showCard(i);
 }
 
+function saveChanges(i) {
+let title = getId('backlog-title').value;
+let description = getId('backlog-description').value;
+let date = getId('backlog-date').value;
 
-function emptyForm() {
-    let assignedToBacklog = getId('assigned-to-backlog');
-    assignedToBacklog.innerHTML = addUserHtml();
-    fillAssignedToListBacklog();
+backlogTasks[i].title = title;
+backlogTasks[i].description = description;
+backlogTasks[i].dueDate = date;
+
+save(allTasks, 'tasks');
+renderCards();
 }
 
 
-function addUserHtml() {
-    return `<div class="btn-group dropend">
-                <img id="plus-icon-backlog" type="button" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" src="img/icon plus.png" alt="add team member" title="add team member">
 
-                <ul id="assigned-to-list-backlog" class="dropdown-menu p-2">
-                </ul>
-            </div>`
-}
+/* <div id="assigned-to-backlog">
+<div class="btn-group dropend">
+    <img onclick="emptyForm()" id="plus-icon-backlog" type="button" class="dropdown-toggle" data-bs-toggle="dropdown"
+        aria-expanded="false" src="img/icon plus.png" alt="add team member"
+        title="add team member">
 
-function fillAssignedToListBacklog() {
-    let list = getId('assigned-to-list-backlog');
-    list.innerHTML = '';
-    for (const name in users) {
-            list.innerHTML += itemMaker(users[name].name);
-        }
-    }
+    <ul id="assigned-to-list-backlog" class="dropdown-menu p-3">
+    </ul>
+</div>
+</div> */
 
-    function assignUser(name) {
-        currentTask.assignedTo.push(name);
-        showAssignedUsers();
-    }
 
-    function showAssignedUsers() {
-        let assignedTo = getId('assigned-to-backlog');
-        assignedTo.innerHTML = '';
-        for (let i = 0; i < currentTask.assignedTo.length; i++) {
-            const name = currentTask.assignedTo[i];
-            assignedTo.innerHTML += staffIconHtml(name);
-        }
+// function emptyForm() {
+//     let assignedToBacklog = getId('assigned-to-backlog');
+//     assignedToBacklog.innerHTML = addUserHtml();
+//     fillAssignedToListBacklog();
+// }
 
-        if (moreStaffAllowed()) {
-            assignedTo.innerHTML += addUserHtml();
-            fillAssignedToList();
-        }
-    }
+
+// function addUserHtml() {
+//     return `<div class="btn-group dropend">
+//                 <img id="plus-icon-backlog" type="button" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" src="img/icon plus.png" alt="add team member" title="add team member">
+
+//                 <ul id="assigned-to-list-backlog" class="dropdown-menu p-2">
+//                 </ul>
+//             </div>`
+// }
+
+// function fillAssignedToListBacklog() {
+//     let list = getId('assigned-to-list-backlog');
+//     list.innerHTML = '';
+//     for (const name in users) {
+//             list.innerHTML += itemMaker(users[name].name);
+//         }
+//     }
+
+//     function assignUser(name) {
+//         currentTask.assignedTo.push(name);
+//         showAssignedUsers();
+//     }
+
+//     function showAssignedUsers() {
+//         let assignedTo = getId('assigned-to-backlog');
+//         assignedTo.innerHTML = '';
+//         for (let i = 0; i < currentTask.assignedTo.length; i++) {
+//             const name = currentTask.assignedTo[i];
+//             assignedTo.innerHTML += staffIconHtml(name);
+//         }
+
+//         if (moreStaffAllowed()) {
+//             assignedTo.innerHTML += addUserHtml();
+//             fillAssignedToList();
+//         }
+//     }
