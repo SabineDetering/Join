cc631a72a8e600df61f3dfc09caac27b3f2d47a7
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-14 12:05:48 +0100

    git bug?

--- js/board.js
+++ js/board.js
@@ -31,7 +31,7 @@ async function renderBoardTasks(onload = false) {
  */
 function taskCard(i) {
     return `    
-    <div id="task${i}" draggable="true" ondragstart="startDragging(${i})" class="card task shadow p-2 mb-1" onclick="showCard(${i})">
+    <div id="task${i}" draggable="true" ondragstart="startDragging(${i})" class="card task shadow p-2 mb-1" onclick="showTask(${i})">
         <h6>${allTasks[i].title}</h6>
 
         <div class="d-flex justify-content-between">
