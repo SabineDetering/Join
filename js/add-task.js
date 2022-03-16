32c8d21d86db1fd298e7de49870b9cbd374ae2ac
Sabine Detering <sabine.detering@web.de>
2022-03-16 23:59:25 +0100

    commented out categories and users hard code

--- js/add-task.js
+++ js/add-task.js
@@ -1,9 +1,9 @@
-categories = ['Backend', 'Frontend', 'Product Owner', 'UI/UX', 'Webdesign'];
-users = {
-    'Christian Aidelsburger': { name: 'Christian Aidelsburger', initials: 'CA', img: './img/chris-icon.png', email: 'c.aidelsburger@web.de', password: '', activeTasks: 0 },
-    'Sabine Detering': { name: 'Sabine Detering', initials: 'SD', img: './img/bee.png', email: 'testmail@web.de', password: '', activeTasks: 0 },
-    'Tuncay Dağdelen': { name: 'Tuncay Dağdelen', initials: 'TD', img: './img/tuncay-icon.png', email: 'muster@email.de', password: 'password', activeTasks: 0 },
-};
+// categories = ['Backend', 'Frontend', 'Product Owner', 'UI/UX', 'Webdesign'];
+// users = {
+//     'Christian Aidelsburger': { name: 'Christian Aidelsburger', initials: 'CA', img: './img/chris-icon.png', email: 'c.aidelsburger@web.de', password: '', activeTasks: 0 },
+//     'Sabine Detering': { name: 'Sabine Detering', initials: 'SD', img: './img/bee.png', email: 'testmail@web.de', password: '', activeTasks: 0 },
+//     'Tuncay Dağdelen': { name: 'Tuncay Dağdelen', initials: 'TD', img: './img/tuncay-icon.png', email: 'muster@email.de', password: 'password', activeTasks: 0 },
+// };
 
 // ////////////////////////////////////////////////////////////////////
 
