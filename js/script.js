ec2e20c0d25714d5fde79578a37e901bd8a21990
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-23 08:20:55 +0100

    Backups moved to readme

--- js/script.js
+++ js/script.js
@@ -9,19 +9,6 @@ let now = new Date();
 let today = now.toISOString().slice(0, 10);
 let timeout = 500;// timeout to prevent simultaneous saving and loading from server
 
-//backup for categories
-/*
-categories = ['Backend', 'Frontend', 'Product Owner', 'UI/UX', 'Webdesign'];
-*/
-
-//backup for users
-/*
-users = {
-    'Christian Aidelsburger': { name: 'Christian Aidelsburger', initials: 'CA', img: './img/chris-icon.png', email: 'c.aidelsburger@web.de', password: '', activeTasks: 0 },
-    'Sabine Detering': { name: 'Sabine Detering', initials: 'SD', img: './img/bee.png', email: 'testmail@web.de', password: '', activeTasks: 0 },
-    'Tuncay Dağdelen': { name: 'Tuncay Dağdelen', initials: 'TD', img: './img/tuncay-icon.png', email: 'muster@email.de', password: 'password', activeTasks: 0 },
-};
-*/
 
 /////////////////////////////////////////////////////////////////////////////////////////////// 
 /* helper functions */
